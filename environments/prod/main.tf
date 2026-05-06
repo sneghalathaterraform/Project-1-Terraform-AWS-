f1b5e1c2d3d3660671c7c8d3b0d0b39e23f2918e
sneghalathaterraform <santhoshonetouch29@gmail.com>
2026-05-06 00:05:33 -0400

    HCP cloud approach - environment folders setup

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,17 +1,17 @@
 module "vpc" {
-  source  ="../../modules/vpc"
+  source     = "../modules/vpc"
   cidr_block = var.vpc_cidr_block
   env        = var.environment
   project    = var.project
 }
 module "iam" {
-  source      = "../../modules/iam"
+  source      = "../modules/iam"
   project     = var.project
   env         = var.environment
   bucket_name = var.bucket_name
 }
 module "ec2" {
-  source = "../../modules/ec2"
+  source               = "../modules/ec2"
   instance_type        = var.instance_type
   env                  = var.environment
   aws_region           = var.aws_region
@@ -20,7 +20,7 @@ module "ec2" {
   iam_instance_profile = module.iam.ec2_instance_profile_name
 }
 module "s3" {
-  source = "../../modules/s3"
+  source      = "../modules/s3"
   bucket_name = var.bucket_name
   env         = var.environment
   project     = var.project
